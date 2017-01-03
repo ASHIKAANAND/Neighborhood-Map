bcb8b58ed805cb76c739d3f7ce239445f80731f3
Anakha07 <anujeeva.16@gmail.com>
2017-01-03 22:33:47 +0530

    Update starter.js

--- js/starter.js
+++ js/starter.js
@@ -1,4 +1,13 @@
+//TO DO:
+//Live filtering of markers
+//street view
+//third party api
+
+'use strict';
+
 var map; //create map as a global variable so that it is available inside the viewModel
+var icon = "images/pin.png";
+var defaultIcon = "images/redpin.png";
 
 // MODEL
 var Model = [
@@ -8,14 +17,16 @@ var Model = [
         location: {
             lat: 9.9825798,
             lng: 76.27542749999998
-        }
+        },
+        id: 1
     },
     {
         title: 'Subash Park',
         location: {
             lat: 9.971488299999999,
             lng: 76.27940949999993
-        }
+        },
+        id: 2
     }
 
 
@@ -25,18 +36,26 @@ var Model = [
 var ViewModel = function() {
     // functions to add markers, show data, filter locations, update infowindow content etc.
     // Run API calls to get data
-    var largeInfowindow = new google.map.InfoWindow(); //initialise the infowindow
+    var largeInfowindow = new google.maps.InfoWindow(); //initialise the infowindow
     var markers = [];
     var self = this;
+    self.filter = ko.observable('');
+
+
     //making the array containing the list of places an observable one
     self.placesList = ko.observableArray(Model);
+
+
+
+
     //creating a marker for each place
     self.placesList().forEach(function(place) {
 
         var marker = new google.maps.Marker({
             map: map,
             position: place.location,
-            title: place.title
+            title: place.title,
+            icon: defaultIcon
 
         });
 
@@ -46,9 +65,46 @@ var ViewModel = function() {
         //creating infowindow for each marker
 
         marker.addListener('click', function() {
-            populateInfoWindow(self, largeInfowindow);
+            populateInfoWindow(place.marker, largeInfowindow);
         });
+
+        //marker animations while hovering
+        google.maps.event.addListener(place.marker, 'mouseover', function() {
+            marker.setIcon(icon);
+        });
+        google.maps.event.addListener(place.marker, 'mouseout', function() {
+            marker.setIcon(defaultIcon);
+        });
+
     })
+
+    //when an item in the list is clicked, the corresponding marker bounces and opens the infowindow
+    self.markerBounce = function(placeClicked) {
+        google.maps.event.trigger(placeClicked.marker, 'click');
+        placeClicked.marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+            placeClicked.marker.setAnimation(null);
+        }, 1000);
+
+    };
+    //live filtering function
+    //1. If the filter holds no value, then the entire list is displayed
+    //2. When the user enters a value, the whole placesList array and the entered value is passed
+    //   the matched place
+    self.filteredTitles = ko.computed(function() {
+        var filter = self.filter().toLowerCase();
+        if (!filter) {
+            return self.placesList();
+        } else {
+            return ko.utils.arrayFilter(self.placesList(), function(place) {
+                return place.title.toLowerCase().indexOf(filter) !== -1;
+
+            });
+        }
+    });
+
+
+
 
     function populateInfoWindow(marker, infowindow) {
         // Check to make sure the infowindow is not already opened on this marker.
@@ -63,7 +119,36 @@ var ViewModel = function() {
         }
     }
 
-};
+    var streetViewService = new google.maps.StreetViewService();
+    var radius = 50;
+
+    function getStreetView(data, status) {
+             if (status == google.maps.StreetViewStatus.OK) {
+               var nearStreetViewLocation = data.location.latLng;
+               var heading = google.maps.geometry.spherical.computeHeading(
+                 nearStreetViewLocation, marker.position);
+                 infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
+                 var panoramaOptions = {
+                   position: nearStreetViewLocation,
+                   pov: {
+                     heading: heading,
+                     pitch: 30
+                   }
+                 };
+               var panorama = new google.maps.StreetViewPanorama(
+                 document.getElementById('pano'), panoramaOptions);
+             } else {
+               infowindow.setContent('<div>' + marker.title + '</div>' +
+                 '<div>No Street View Found</div>');
+             }
+           }
+           // Use streetview service to get the closest streetview image within
+           // 50 meters of the markers position
+           streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+           // Open the infowindow on the correct marker.
+           infowindow.open(map, marker);
+
+       };
 
 //Function to load map and start up app
 var initMap = function() {
@@ -201,12 +286,13 @@ var initMap = function() {
                     color: '#17263c'
                 }]
             }
-        ]
+        ],
 
     });
 
     ko.applyBindings(new ViewModel);
     //Instantiate ViewModel
+
 };
 
 //ko.applyBindings(new ViewModel());
